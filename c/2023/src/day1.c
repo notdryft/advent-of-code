58f5ebff82ef0f0cf01c052f6c2df08156284f74
Guillaume Corré <gui.corre@gmail.com>
2023-12-02 01:22:37 +0100

    chore: clean up

--- c/2023/src/day1.c
+++ c/2023/src/day1.c
@@ -17,15 +17,15 @@ int part1(char *filename) {
 
   char buffer[MAX_LENGTH];
   while (fgets(buffer, MAX_LENGTH, fp)) {
-    Array *array = array_new();
+    Array *digits = array_new();
     for (int i = 0; buffer[i] != '\n'; i++) {
       int value = buffer[i] - '0';
       if (value > 0 && value < 10) {
-        array_push(array, value);
+        array_push(digits, value);
       }
     }
-    sum += array_first(array) * 10 + array_last(array);
-    array_free(array);
+    sum += array_first(digits) * 10 + array_last(digits);
+    array_free(digits);
   }
 
   printf("sum: %d\n", sum);
@@ -46,29 +46,29 @@ int part2(char *filename) {
 
   char buffer[MAX_LENGTH];
   while (fgets(buffer, MAX_LENGTH, fp)) {
-    Array *array = array_new();
+    Array *digits = array_new();
     size_t len = strlen(buffer) - 1; // /n counts but not \0
     for (size_t i = 0; buffer[i] != '\n'; i++) {
       char c = buffer[i];
       if (c >= '1' && c <= '9') {
-        array_push(array, c - '0');
+        array_push(digits, c - '0');
       } else {
         // one two three four five six seven eight nine
         // o   t         f         s         e     n
-        if (c == 'o' && (i+2) < len && buffer[i+1] == 'n' && buffer[i+2] == 'e') array_push(array, 1);
-        if (c == 't' && (i+2) < len && buffer[i+1] == 'w' && buffer[i+2] == 'o') array_push(array, 2);
-        if (c == 't' && (i+4) < len && buffer[i+1] == 'h' && buffer[i+2] == 'r' && buffer[i+3] == 'e' && buffer[i+4] == 'e') array_push(array, 3);
-        if (c == 'f' && (i+3) < len && buffer[i+1] == 'o' && buffer[i+2] == 'u' && buffer[i+3] == 'r') array_push(array, 4);
-        if (c == 'f' && (i+3) < len && buffer[i+1] == 'i' && buffer[i+2] == 'v' && buffer[i+3] == 'e') array_push(array, 5);
-        if (c == 's' && (i+2) < len && buffer[i+1] == 'i' && buffer[i+2] == 'x') array_push(array, 6);
-        if (c == 's' && (i+4) < len && buffer[i+1] == 'e' && buffer[i+2] == 'v' && buffer[i+3] == 'e' && buffer[i+4] == 'n') array_push(array, 7);
-        if (c == 'e' && (i+4) < len && buffer[i+1] == 'i' && buffer[i+2] == 'g' && buffer[i+3] == 'h' && buffer[i+4] == 't') array_push(array, 8);
-        if (c == 'n' && (i+3) < len && buffer[i+1] == 'i' && buffer[i+2] == 'n' && buffer[i+3] == 'e') array_push(array, 9);
+        if (c == 'o' && (i+2) < len && buffer[i+1] == 'n' && buffer[i+2] == 'e') array_push(digits, 1);
+        if (c == 't' && (i+2) < len && buffer[i+1] == 'w' && buffer[i+2] == 'o') array_push(digits, 2);
+        if (c == 't' && (i+4) < len && buffer[i+1] == 'h' && buffer[i+2] == 'r' && buffer[i+3] == 'e' && buffer[i+4] == 'e') array_push(digits, 3);
+        if (c == 'f' && (i+3) < len && buffer[i+1] == 'o' && buffer[i+2] == 'u' && buffer[i+3] == 'r') array_push(digits, 4);
+        if (c == 'f' && (i+3) < len && buffer[i+1] == 'i' && buffer[i+2] == 'v' && buffer[i+3] == 'e') array_push(digits, 5);
+        if (c == 's' && (i+2) < len && buffer[i+1] == 'i' && buffer[i+2] == 'x') array_push(digits, 6);
+        if (c == 's' && (i+4) < len && buffer[i+1] == 'e' && buffer[i+2] == 'v' && buffer[i+3] == 'e' && buffer[i+4] == 'n') array_push(digits, 7);
+        if (c == 'e' && (i+4) < len && buffer[i+1] == 'i' && buffer[i+2] == 'g' && buffer[i+3] == 'h' && buffer[i+4] == 't') array_push(digits, 8);
+        if (c == 'n' && (i+3) < len && buffer[i+1] == 'i' && buffer[i+2] == 'n' && buffer[i+3] == 'e') array_push(digits, 9);
       }
     }
-    sum += array_first(array) * 10 + array_last(array);
-    array_print(array);
-    array_free(array);
+    sum += array_first(digits) * 10 + array_last(digits);
+    array_print(digits);
+    array_free(digits);
   }
 
   printf("sum: %d\n", sum);
