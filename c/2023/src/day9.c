4eee25848001213bead5e4710f85197db4434021
Guillaume Corré <gui.corre@gmail.com>
2024-01-20 13:28:48 +0100

    fix: memory leaks on day 9

--- c/2023/src/day9.c
+++ c/2023/src/day9.c
@@ -16,6 +16,7 @@ int predict_first_rec(Array *array) {
   debugf(int_array_print, sub);
   if (int_array_all(sub, 0)) {
     int first = int_array_first(array);
+    array_free(sub);
     debug("array first = %d, sub array predirection = 0, prediction = %d\n", first, first);
     return first;
   } else {
@@ -37,12 +38,14 @@ int predict_last_rec(Array *array) {
   if (int_array_all(sub, 0)) {
     int last = int_array_last(array);
     debug("array last = %d, sub array predirection = 0, prediction = %d\n", last, last);
+    array_free(sub);
     return last;
   } else {
     int sub_prediction = predict_last_rec(sub);
     int last = int_array_last(array);
     int prediction = last + sub_prediction;
     debug("array last = %d, sub array predirection = %d, prediction = %d\n", last, sub_prediction, prediction);
+    array_free(sub);
     return prediction;
   }
 }
@@ -74,6 +77,7 @@ int part2(StringArray *lines) {
     debugf(int_array_print, array);
 
     int first = predict_first_rec(array);
+    array_free(array);
     result += first;
   }
 
