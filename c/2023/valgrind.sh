692484ac47c2522e3f726074613255ada122599b
Guillaume Corré <gui.corre@gmail.com>
2024-01-20 13:23:10 +0100

    feat: add debug flags to valgrind script

--- c/2023/valgrind.sh
+++ c/2023/valgrind.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
 
+args="$(getopt -o dit --long debug,info,trace -- "$@")"
+eval set -- "$args"
+
+while true; do
+  case "$1" in
+    -d | --debug) debug=true; shift ;;
+    -i | --info) info=true;   shift ;;
+    -t | --trace) trace=true; shift ;;
+    * )                       shift  ; break ;;
+  esac
+done
+
 if [[ $1 =~ [0-9]+ ]]; then
   binary="day$1"
 else
   binary="${1}_test"
 fi
+
+if [ "$trace" = "true" ]; then
+  flags=-DTRACE
+elif [ "$debug" = "true" ]; then
+  flags=-DDEBUG
+elif [ "$info" = "true" ]; then
+  flags=-DINFO
+fi
+
+make_flags=CMDLINE_FLAGS=$flags
 
 case $(uname -s) in
   Darwin)
@@ -13,9 +35,9 @@ case $(uname -s) in
       --platform linux/amd64 \
       --volume "$(pwd)/../..:/app" \
       valgrind:latest \
-      bash -c "make mrproper && make mem-$binary"
+      bash -c "make mrproper && make mem-$binary $make_flags"
     ;;
   Linux)
-    make mrproper && make "mem-$binary"
+    make mrproper && make "mem-$binary" "$make_flags"
     ;;
 esac
