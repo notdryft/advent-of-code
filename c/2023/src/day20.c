de97cd4cb6175527e5699fc38172f504d5220a01
Guillaume Corré <gui.corre@gmail.com>
2024-01-21 15:44:22 +0100

    fix: memory leaks and random results on day 20

--- c/2023/src/day20.c
+++ c/2023/src/day20.c
@@ -19,12 +19,11 @@ enum ModuleType {
 };
 
 typedef struct {
-  char name[64];
+  char *name;
   enum ModuleType module_type;
   bool on;
-  size_t connections_size;
-  char connections[10][64];
-  int memory[26*27];
+  StringArray *connections;
+  int *memory;
   Array *inputs;
 } Module;
 
@@ -46,14 +45,14 @@ void signal_print(Signal *signal) {
 void module_print(Module *module) {
   char *module_type = module->module_type == 0 ? " ?" : (module->module_type == 1 ? " " : (module->module_type == 2 ? " %" : " &"));
   printf("{%s%s ", module_type, module->name);
-  if (module->connections_size > 0) {
+  if (module->connections->size > 0) {
     printf("-> ");
   } else {
     printf("X");
   }
-  for (size_t i = 0; i < module->connections_size; i++) {
-    printf("%s", module->connections[i]);
-    if (i < module->connections_size - 1) printf(", ");
+  for (size_t i = 0; i < module->connections->size; i++) {
+    printf("%s", module->connections->items[i]);
+    if (i < module->connections->size - 1) printf(", ");
   }
   if (module->module_type == CONJUNCTION) {
     printf(" [ ");
@@ -94,73 +93,68 @@ Module *find_module(Array *modules, char *name) {
   return nullptr;
 }
 
-Module *find_or_init_module(Array *modules, char *name) {
-  Module *module = find_module(modules, name);
-  if (module == nullptr) {
-    Module tmp = {};
-    strncpy(tmp.name, name, 63);
-    array_push(modules, &tmp);
-
-    module = find_module(modules, name);
-    module->module_type = OUTPUT;
-    module->connections_size = 0;
-    for (size_t i = 0; i < 10; i++) {
-      memset(module->connections[i], 0, 64);
-    }
-    memset(module->memory, 0, sizeof(int) * 26 * 27);
-    module->inputs = array_new(size_t);
-  }
+Module init_module(char *name) {
+  Module module = {
+    .name = strdup(name),
+    .connections = string_array_new(),
+    .memory = calloc(26 * 27, sizeof(*module.memory)),
+    .inputs = array_new(size_t)
+  };
 
   return module;
 }
 
 unsigned long long part1(StringArray *lines) {
   Array *modules = array_new(Module);
-  Module *broadcast = find_or_init_module(modules, "broadcaster");
-  broadcast->module_type = BROADCASTER;
+  string_array_foreach (char *line, lines) {
+    StringArray *split = string_split(line, " -> ");
+    char *name = split->items[0];
+    if (name[0] == '%' || name[0] == '&') {
+      name = name + 1;
+    }
+
+    Module module = init_module(name);
+    if (line[0] == '%') {
+      module.module_type = FLIP_FLOP;
+    } else if (line[0] == '&') {
+      module.module_type = CONJUNCTION;
+    } else {
+      module.module_type = BROADCASTER;
+    }
+    array_push(modules, &module);
+
+    string_array_free(split);
+  }
+  debugf(array_module_print, modules);
 
   string_array_foreach (char *line, lines) {
-    if (strncmp(line, "broadcaster", 11) == 0 || line[0] == '%' || line[0] == '&') {
-      StringArray *split = string_split(line, " -> ");
-      StringArray *csplit = string_split(split->items[1], ", ");
-      if (strncmp(line, "broadcaster", 11) == 0) {
-        debugf(array_module_print, modules);
-        for (size_t i = 0; i < csplit->size; i++) {
-          char *name = csplit->items[i];
-          strncpy(broadcast->connections[broadcast->connections_size++], name, 64);
-          debugf(array_module_print, modules);
-        }
-      } else if (line[0] == '%') {
-        Module *fp = find_or_init_module(modules, split->items[0] + 1);
-        fp->module_type = FLIP_FLOP;
-        debugf(array_module_print, modules);
-        for (size_t i = 0; i < csplit->size; i++) {
-          strncpy(fp->connections[fp->connections_size++], csplit->items[i], 64);
-          debugf(array_module_print, modules);
-        }
-      } else if (line[0] == '&') {
-        Module *conj = find_or_init_module(modules, split->items[0] + 1);
-        conj->module_type = CONJUNCTION;
-        debugf(array_module_print, modules);
-        for (size_t i = 0; i < csplit->size; i++) {
-          strncpy(conj->connections[conj->connections_size++], csplit->items[i], 64);
-          find_or_init_module(modules, csplit->items[i]);
-          debugf(array_module_print, modules);
-        }
-      }
-
-      string_array_free(csplit);
-      string_array_free(split);
-    } else {
-      printf("unlucky\n");
-      exit(42);
+    StringArray *split = string_split(line, " -> ");
+    char *name = split->items[0];
+    if (name[0] == '%' || name[0] == '&') {
+      name = name + 1;
     }
+
+    StringArray *csplit = string_split(split->items[1], ", ");
+
+    Module *module = find_module(modules, name);
+    string_array_foreach(char *connection, csplit) {
+      Module *c = find_module(modules, connection);
+      if (c == nullptr) {
+        Module output = init_module(connection);
+        array_push(modules, &output);
+      }
+      string_array_push(module->connections, connection);
+    }
+
+    string_array_free(csplit);
+    string_array_free(split);
   }
+  debugf(array_module_print, modules);
 
   for (size_t i = 0; i < modules->size; i++) {
     Module *m = array_get(modules, i);
-    for (size_t j = 0; j < m->connections_size; j++) {
-      Module *c = find_module(modules, m->connections[j]);
+    for (size_t j = 0; j < m->connections->size; j++) {
+      Module *c = find_module(modules, m->connections->items[j]);
       if (c->module_type == CONJUNCTION) {
         array_push_rval(c->inputs, hash(m->name));
       }
@@ -174,26 +168,24 @@ unsigned long long part1(StringArray *lines) {
 
   for (size_t i = 0; i < times; i++) {
     Array *q = array_new(Signal);
-    array_push(q, &(Signal) { .source = nullptr, .signal_type = LOW, .destination = broadcast });
+    array_push(q, &(Signal) { .source = nullptr, .signal_type = LOW, .destination = find_module(modules, "broadcaster") });
     while (q->size > 0) {
       Signal *s = array_pop(q);
       if (s->destination->module_type == OUTPUT) {
-        free(s);
-        continue;
+        goto defer;
       } else if (s->destination->module_type == BROADCASTER) {
-        debugf(signal_print, s);
+        tracef(signal_print, s);
         lows++;
-        for (size_t j = 0; j < s->destination->connections_size; j++) {
+        for (size_t j = 0; j < s->destination->connections->size; j++) {
           lows++;
-          Module *m = find_module(modules, s->destination->connections[j]);
+          Module *m = find_module(modules, s->destination->connections->items[j]);
           Signal ns = { .source = s->destination, .signal_type = LOW, .destination = m };
-          debugf(signal_print, &ns);
+          tracef(signal_print, &ns);
           array_push(q, &ns);
         }
       } else if (s->destination->module_type == FLIP_FLOP) {
         if (s->signal_type == HIGH) {
-          free(s);
-          continue;
+          goto defer;
         }
         s->destination->on = !s->destination->on;
         enum SignalType st;
@@ -202,12 +194,12 @@ unsigned long long part1(StringArray *lines) {
         } else {
           st = LOW;
         }
-        for (size_t j = 0; j < s->destination->connections_size; j++) {
-          Module *m = find_module(modules, s->destination->connections[j]);
+        for (size_t j = 0; j < s->destination->connections->size; j++) {
+          Module *m = find_module(modules, s->destination->connections->items[j]);
           Signal ns = { .source = s->destination, .signal_type = st, .destination = m };
           if (st == LOW) lows++;
           else highs++;
-          debugf(signal_print, &ns);
+          tracef(signal_print, &ns);
           array_push(q, &ns);
         }
       } else if (s->destination->module_type == CONJUNCTION) {
@@ -221,15 +213,16 @@ unsigned long long part1(StringArray *lines) {
 
         enum SignalType st = all_high ? LOW : HIGH;
 
-        for (size_t j = 0; j < s->destination->connections_size; j++) {
-          Module *m = find_module(modules, s->destination->connections[j]);
+        for (size_t j = 0; j < s->destination->connections->size; j++) {
+          Module *m = find_module(modules, s->destination->connections->items[j]);
           Signal ns = { .source = s->destination, .signal_type = st, .destination = m };
           if (st == LOW) lows++;
           else highs++;
-          debugf(signal_print, &ns);
+          tracef(signal_print, &ns);
           array_push(q, &ns);
         }
       }
+      defer:
       free(s);
     }
     array_free(q);
@@ -241,6 +234,9 @@ unsigned long long part1(StringArray *lines) {
 
   for (size_t i = 0; i < modules->size; i++) {
     Module *module = array_get(modules, i);
+    free(module->name);
+    string_array_free(module->connections);
+    free(module->memory);
     array_free(module->inputs);
   }
   array_free(modules);
@@ -250,52 +246,56 @@ unsigned long long part1(StringArray *lines) {
 
 long long part2(StringArray *lines) {
   Array *modules = array_new(Module);
-  Module *broadcast = find_or_init_module(modules, "broadcaster");
-  broadcast->module_type = BROADCASTER;
+  string_array_foreach (char *line, lines) {
+    StringArray *split = string_split(line, " -> ");
+    char *name = split->items[0];
+    if (name[0] == '%' || name[0] == '&') {
+      name = name + 1;
+    }
+
+    Module module = init_module(name);
+    if (line[0] == '%') {
+      module.module_type = FLIP_FLOP;
+    } else if (line[0] == '&') {
+      module.module_type = CONJUNCTION;
+    } else {
+      module.module_type = BROADCASTER;
+    }
+    array_push(modules, &module);
+
+    string_array_free(split);
+  }
+  debugf(array_module_print, modules);
 
   string_array_foreach (char *line, lines) {
-    if (strncmp(line, "broadcaster", 11) == 0 || line[0] == '%' || line[0] == '&') {
-      StringArray *split = string_split(line, " -> ");
-      StringArray *csplit = string_split(split->items[1], ", ");
-      if (strncmp(line, "broadcaster", 11) == 0) {
-        debugf(array_module_print, modules);
-        for (size_t i = 0; i < csplit->size; i++) {
-          char *name = csplit->items[i];
-          strncpy(broadcast->connections[broadcast->connections_size++], name, 64);
-          debugf(array_module_print, modules);
-        }
-      } else if (line[0] == '%') {
-        Module *fp = find_or_init_module(modules, split->items[0] + 1);
-        fp->module_type = FLIP_FLOP;
-        debugf(array_module_print, modules);
-        for (size_t i = 0; i < csplit->size; i++) {
-          strncpy(fp->connections[fp->connections_size++], csplit->items[i], 64);
-          debugf(array_module_print, modules);
-        }
-      } else if (line[0] == '&') {
-        Module *conj = find_or_init_module(modules, split->items[0] + 1);
-        conj->module_type = CONJUNCTION;
-        debugf(array_module_print, modules);
-        for (size_t i = 0; i < csplit->size; i++) {
-          strncpy(conj->connections[conj->connections_size++], csplit->items[i], 64);
-          find_or_init_module(modules, csplit->items[i]);
-          debugf(array_module_print, modules);
-        }
-      }
-
-      string_array_free(csplit);
-      string_array_free(split);
-    } else {
-      printf("unlucky\n");
-      exit(42);
+    StringArray *split = string_split(line, " -> ");
+    char *name = split->items[0];
+    if (name[0] == '%' || name[0] == '&') {
+      name = name + 1;
     }
+
+    StringArray *csplit = string_split(split->items[1], ", ");
+
+    Module *module = find_module(modules, name);
+    string_array_foreach(char *connection, csplit) {
+      Module *c = find_module(modules, connection);
+      if (c == nullptr) {
+        Module output = init_module(connection);
+        array_push(modules, &output);
+      }
+      string_array_push(module->connections, connection);
+    }
+
+    string_array_free(csplit);
+    string_array_free(split);
   }
+  debugf(array_module_print, modules);
 
   size_t conjunctions = 4;
   for (size_t i = 0; i < modules->size; i++) {
     Module *m = array_get(modules, i);
-    for (size_t j = 0; j < m->connections_size; j++) {
-      Module *c = find_module(modules, m->connections[j]);
+    for (size_t j = 0; j < m->connections->size; j++) {
+      Module *c = find_module(modules, m->connections->items[j]);
       if (c->module_type == CONJUNCTION) {
         array_push_rval(c->inputs, hash(m->name));
       }
@@ -304,28 +304,25 @@ long long part2(StringArray *lines) {
   debugf(array_module_print, modules);
   debug("conjunctions: %zu\n", conjunctions);
 
-  bool done[26*27];
-  memset(done, 0, 26*27*sizeof(bool));
+  bool done[26*27] = {};
   Array *cycles = array_new(long long);
 
-  long long cycle = 0;
+  long long cycle = 1;
   while (cycles->size < conjunctions) {
     Array *q = array_new(Signal);
-    array_push(q, &(Signal) { .source = nullptr, .signal_type = LOW, .destination = broadcast });
+    array_push(q, &(Signal) { .source = nullptr, .signal_type = LOW, .destination = find_module(modules, "broadcaster") });
     while (q->size > 0) {
       Signal *s = array_pop(q);
       if (s->destination->module_type == OUTPUT) {
-        free(s);
-        continue;
+        goto defer;
       } else if (s->destination->module_type == BROADCASTER) {
-        for (size_t j = 0; j < s->destination->connections_size; j++) {
-          Module *m = find_module(modules, s->destination->connections[j]);
+        for (size_t j = 0; j < s->destination->connections->size; j++) {
+          Module *m = find_module(modules, s->destination->connections->items[j]);
           array_push(q, &(Signal) { .source = s->destination, .signal_type = LOW, .destination = m });
         }
       } else if (s->destination->module_type == FLIP_FLOP) {
         if (s->signal_type == HIGH) {
-          free(s);
-          continue;
+          goto defer;
         }
         s->destination->on = !s->destination->on;
         enum SignalType st;
@@ -334,8 +331,8 @@ long long part2(StringArray *lines) {
         } else {
           st = LOW;
         }
-        for (size_t j = 0; j < s->destination->connections_size; j++) {
-          Module *m = find_module(modules, s->destination->connections[j]);
+        for (size_t j = 0; j < s->destination->connections->size; j++) {
+          Module *m = find_module(modules, s->destination->connections->items[j]);
           array_push(q, &(Signal) { .source = s->destination, .signal_type = st, .destination = m });
         }
       } else if (s->destination->module_type == CONJUNCTION) {
@@ -362,11 +359,12 @@ long long part2(StringArray *lines) {
           }
         }
 
-        for (size_t j = 0; j < s->destination->connections_size; j++) {
-          Module *m = find_module(modules, s->destination->connections[j]);
+        for (size_t j = 0; j < s->destination->connections->size; j++) {
+          Module *m = find_module(modules, s->destination->connections->items[j]);
           array_push(q, &(Signal) { .source = s->destination, .signal_type = st, .destination = m });
         }
       }
+      defer:
       free(s);
     }
     cycle++;
@@ -379,9 +377,13 @@ long long part2(StringArray *lines) {
   for (size_t i = 1; i < cycles->size; i++) {
     result = lcm(result, ll_array_get(cycles, i));
   }
+  array_free(cycles);
 
   for (size_t i = 0; i < modules->size; i++) {
     Module *module = array_get(modules, i);
+    free(module->name);
+    string_array_free(module->connections);
+    free(module->memory);
     array_free(module->inputs);
   }
   array_free(modules);
@@ -390,9 +392,9 @@ long long part2(StringArray *lines) {
 }
 
 int main(void) {
-  //test_case(day20, part1, sample1, 32000000);
-  //test_case(day20, part1, sample2, 11687500);
-  //test_case(day20, part1, data, 681194780); // random result???
+  test_case(day20, part1, sample1, 32000000);
+  test_case(day20, part1, sample2, 11687500);
+  test_case(day20, part1, data, 681194780);
   test_case(day20, part2, data, 238593356738827);
 
   return 0;
