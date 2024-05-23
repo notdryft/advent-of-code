1fcf7cfc2ffbbf210beb031578331cfb194d3f8b
Guillaume Corré <gui.corre@gmail.com>
2024-05-23 16:34:15 +0200

    chore(2015): refactor day 9

--- c/2015/src/day9.c
+++ c/2015/src/day9.c
@@ -25,7 +25,7 @@ void visit(Array *distances, i64 **graph, size_t graph_size, bool *visited, size
   }
 }
 
-i64 part1(StringArray *lines) {
+Array *compute(StringArray *lines) {
   StringArray *destinations = string_array_new();
 
   string_array_foreach (char *line, lines) {
@@ -69,13 +69,26 @@ i64 part1(StringArray *lines) {
 
   bool *visited = calloc(destinations->size, sizeof(*visited));
   Array *distances = array_new(i64);
-  
+
   for (size_t i = 0; i < destinations->size; i++) {
     visited[i] = true;
     visit(distances, graph, destinations->size, visited, i, 0);
     visited[i] = false;
   }
   debugf(i64_array_print, distances);
+
+  for (size_t j = 0; j < destinations->size; j++) {
+    free(graph[j]);
+  }
+  free(graph);
+  free(visited);
+  string_array_free(destinations);
+
+  return distances;
+}
+
+i64 part1(StringArray *lines) {
+  Array *distances = compute(lines);
 
   i64 result = INT64_MAX;
   array_foreach(i64 *distance, distances) {
@@ -83,69 +96,13 @@ i64 part1(StringArray *lines) {
       result = *distance;
     }
   }
-
-  for (size_t j = 0; j < destinations->size; j++) {
-    free(graph[j]);
-  }
-  free(graph);
-  free(visited);
   array_free(distances);
-  string_array_free(destinations);
 
   return result;
 }
 
 i64 part2(StringArray *lines) {
-  StringArray *destinations = string_array_new();
-
-  string_array_foreach (char *line, lines) {
-    char source[14], destination[14];
-    if (sscanf(line, "%s to %s = %*d", source, destination) == 2) {
-      if (!string_array_contains(destinations, source)) {
-        string_array_push(destinations, source);
-      }
-      if (!string_array_contains(destinations, destination)) {
-        string_array_push(destinations, destination);
-      }
-    }
-  }
-  debugf(string_array_print, destinations);
-
-  i64 **graph = calloc(destinations->size, sizeof(*graph));
-  for (size_t j = 0; j < destinations->size; j++) {
-    graph[j] = calloc(destinations->size, sizeof(**graph));
-  }
-
-  string_array_foreach (char *line, lines) {
-    i64 distance;
-    char source[14], destination[14];
-    if (sscanf(line, "%s to %s = %lld", source, destination, &distance) == 3) {
-      FindResult rs = string_array_find(destinations, source);
-      FindResult rd = string_array_find(destinations, destination);
-      if (rs.found && rd.found) {
-        graph[rs.index][rd.index] = distance;
-        graph[rd.index][rs.index] = distance;
-      }
-    }
-  }
-#ifdef DEBUG
-  for (size_t j = 0; j < destinations->size; j++) {
-    for (size_t i = 0; i < destinations->size; i++) {
-      printf("%lld\t", graph[j][i]);
-    }
-    printf("\n");
-  }
-#endif
-
-  bool *visited = calloc(destinations->size, sizeof(*visited));
-  Array *distances = array_new(i64);
-  
-  for (size_t i = 0; i < destinations->size; i++) {
-    visited[i] = true;
-    visit(distances, graph, destinations->size, visited, i, 0);
-    visited[i] = false;
-  }
-  debugf(i64_array_print, distances);
+  Array *distances = compute(lines);
 
   i64 result = -1;
   array_foreach(i64 *distance, distances) {
@@ -153,14 +110,7 @@ i64 part2(StringArray *lines) {
       result = *distance;
     }
   }
-
-  for (size_t j = 0; j < destinations->size; j++) {
-    free(graph[j]);
-  }
-  free(graph);
-  free(visited);
   array_free(distances);
-  string_array_free(destinations);
 
   return result;
 }
